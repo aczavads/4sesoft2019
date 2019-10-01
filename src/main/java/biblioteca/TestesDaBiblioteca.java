bfeb157da511f38820304562d8cb919692e78aba
arthur.zavadski <aczavads@gmail.com>
2019-10-01 20:28:41 -0300

    Novos testes

--- src/main/java/biblioteca/TestesDaBiblioteca.java
+++ src/main/java/biblioteca/TestesDaBiblioteca.java
@@ -9,9 +9,28 @@ public class TestesDaBiblioteca {
 	@Test
 	public void testarSeBibliotecaTemAcervo() {
 		Biblioteca uniCesumarSede = new Biblioteca("UniCesumar Campus Sede");
-		uniCesumarSede.addAcervo(new Acervo("Acervo Sede Maringá"));
-		uniCesumarSede.addAcervo(new Acervo("Acervo Medicina"));		
-		assertEquals(2, uniCesumar.getAcervos().size());
+
+		Acervo acervo01 = new Acervo("Acervo Sede Maringá");
+		uniCesumarSede.addAcervo(acervo01);
+		
+		Acervo acervo02 = new Acervo("Acervo Medicina");
+		uniCesumarSede.addAcervo(acervo02);
+		
+		assertEquals("Deveria ter 2!!!", 2, uniCesumarSede.getAcervos().size());		
+		assertTrue("Deveria ter o Acervo Sede Maringá!", uniCesumarSede.getAcervos().contains(acervo01));
+		assertTrue("Deveria ter o Acervo Medicina", uniCesumarSede.getAcervos().contains(acervo02));
 	}
+	
+	@Test(expected=RuntimeException.class)
+	public void testarExceçãoNomeNuloAoConstruirBiblioteca() {
+		Biblioteca nova = new Biblioteca(null);		
+	}
+	
+	@Test
+	public void testarSeNomeBibliotecaEhNulo() {
+		Biblioteca uniCesumarSede = new Biblioteca("UniCesumar Campus Sede");
+		assertNotNull("Nome da biblioteca não pode ser nulo!", uniCesumarSede.getNome());
+	}
+
 
 }
